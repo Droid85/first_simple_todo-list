1277b8320fdf69a5eacf273090f7bb24e3db5fc7
Andrii Ostapenko <droid85@yahoo.com>
2021-09-26 21:32:50 +0300

    complit onClick function

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const buttonEl = document.querySelector('#add-todo-btn');
 const inputEl = document.querySelector('#input-text');
 const listEl = document.querySelector('#todo-list');
-const listElementEl = document.createElement('li');
 
 buttonEl.addEventListener('click', onClick);
 
 function onClick() {
-        listElementEl.textContent = inputEl.value;
-        listEl.append(listElementEl);
-        inputEl.value = '';
+	let listElementEl = document.createElement('li');
+	listElementEl.textContent = inputEl.value;
+	listEl.append(listElementEl);
+	inputEl.value = '';
 }
